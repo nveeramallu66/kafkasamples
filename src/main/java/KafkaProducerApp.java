13e8479273e0606542501d389749467118370216
nveeramallu66 <veernag1991@gmail.com>
2021-01-17 19:50:33 -0500

    Initial Commit : Basic Kafka Producer

---
+++ src/main/java/KafkaProducerApp.java
@@ -0,0 +1,27 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class KafkaProducerApp {
+    public static void main(String[] args){
+        // Create a properties dictionary for the required / optional Producer config settings:
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        // --> props.put("config.setting", "value");
+        // :: http://kafka.apache.org/documentation.html#producerconfigs
+
+        KafkaProducer<String, String> myProducer = new KafkaProducer<String, String>(props);
+        try {
+            for (int i = 0; i < 150; i++ )
+                myProducer.send(new ProducerRecord<String, String>("quickstart-events", Integer.toString(i), "MyMessage: "+Integer.toString(i)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            myProducer.close();
+        }
+    }
+
+}
